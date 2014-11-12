6bbc823bbcae73539130e72d5481027e851380a8
Krzysztof Profic <kprofic@gmail.com>
2014-11-12 13:35:36 +0100

    Introduced TFLogger support.

---
+++ Example/Pods/Target Support Files/Pods-TFLayoutConstraintsFactory-TFLogger/Pods-TFLayoutConstraintsFactory-TFLogger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TFLayoutConstraintsFactory_TFLogger : NSObject
+@end
+@implementation PodsDummy_Pods_TFLayoutConstraintsFactory_TFLogger
+@end
--- Example/Pods/Target Support Files/Pods-TFLayoutConstraintsFactory/Pods-TFLayoutConstraintsFactory-environment.h
+++ Example/Pods/Target Support Files/Pods-TFLayoutConstraintsFactory/Pods-TFLayoutConstraintsFactory-environment.h
@@ -12,3 +12,15 @@
 #define COCOAPODS_VERSION_MINOR_TFLayoutConstraintsFactory 0
 #define COCOAPODS_VERSION_PATCH_TFLayoutConstraintsFactory 1
 
+// TFLogger/Core
+#define COCOAPODS_POD_AVAILABLE_TFLogger_Core
+#define COCOAPODS_VERSION_MAJOR_TFLogger_Core 1
+#define COCOAPODS_VERSION_MINOR_TFLogger_Core 0
+#define COCOAPODS_VERSION_PATCH_TFLogger_Core 0
+
+// TFLogger/NSLogVisualFormat
+#define COCOAPODS_POD_AVAILABLE_TFLogger_NSLogVisualFormat
+#define COCOAPODS_VERSION_MAJOR_TFLogger_NSLogVisualFormat 1
+#define COCOAPODS_VERSION_MINOR_TFLogger_NSLogVisualFormat 0
+#define COCOAPODS_VERSION_PATCH_TFLogger_NSLogVisualFormat 0
+
